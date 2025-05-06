c00973638881513e4715b526af22b3e70661f03e
A2va <49582555+A2va@users.noreply.github.com>
2025-05-06 15:56:00 +0200

    Add script for task 5

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -17,17 +17,22 @@ fi
 
 # Search for problematic files
 echo "The following files/directories are world-writable:"
-file_to_fix=$(find "$dir" -perm -o=w)
-echo "$file_to_fix"
+files_to_fix=$(find "$dir" -perm -o=w)
+echo "$files_to_fix"
 
 echo "Do you want the permissions to be fixed (y/n)?"
 read -r answer
 if [[ $answer == "y" ]]; then
     echo "Fixing permissions..."
     # This is a complex line just to support folders with spaces inside
-    # we didn't find a way to split the $file_to_fix output by \n but not by space.
-    echo "$file_to_fix" | xargs -I _ chmod o-w _
+    # we didn't find a way to split the $files_to_fix output by \n but not by space.
+    echo "$files_to_fix" | xargs -I _ chmod o-w _
 else
     echo Canceled action
 fi
 
+echo "The following files/directories are writable for groups:"
+
+# List all file/directory that are group writable but not by the user group
+files=$(find "$dir" -perm -g=w ! -group "$USER")
+echo "$files"
